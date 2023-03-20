bbd55fcbe809d874fa5ca6f8efcc64e41db94370
Lukasz8808 <53368819+Lukasz8808@users.noreply.github.com>
2023-03-20 18:56:34 +0100

    Add files via upload

---
+++ Backend-main/README.md
@@ -0,0 +1,9 @@
+# project-backend
+
+To run the project, follow these steps:
+
+Create an image by typing "docker build . -t image_name" in the terminal.
+Once the image is created, run it on port "8000".
+For Docker Desktop users, go to the "Images" tab, select the image, and click the "run" button.
+Expand the "Optional Settings" menu and enter "8000" in the "Ports" field, than click "Run".
+A terminal window will appear and prompt you to select the first option and the application should now be ready for use.
---
+++ Backend-main/hello.py
@@ -0,0 +1,33 @@
+"""
+Flask endpoints.
+"""
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    """
+    high level support for doing this and that.
+    """
+    return '<p>Hello, World!</p>'
+
+
+@app.route('/hello')
+def hello_with_name():
+    """
+    high level support for doing this and that.
+    """
+    return render_template('home.html')
+
+
+@app.route('/namesake')
+def greetings():
+    """
+    high level support for doing this and that.
+    """
+    return render_template('namesake.html', name=request.args['name'])
+
+if __name__ == "__main__":
+    hello.run(debug=True, host='0.0.0.0', port=8000)
